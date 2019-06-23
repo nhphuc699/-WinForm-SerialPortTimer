cb6ee5425b7f76cc6f59cfd097e0b2d0e524f446
nhphuc699 <39647546+nhphuc699@users.noreply.github.com>
2019-06-23 12:08:23 +0700

    Code GetDataForW100

--- [WinForm]SerialPortTimer/Form1.Designer.cs
+++ [WinForm]SerialPortTimer/Form1.Designer.cs
@@ -35,11 +35,19 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(69, 77);
+            this.button1.Location = new System.Drawing.Point(69, 85);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -74,9 +82,9 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(33, 147);
+            this.richTextBox1.Location = new System.Drawing.Point(33, 187);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(354, 257);
+            this.richTextBox1.Size = new System.Drawing.Size(541, 244);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
@@ -85,11 +93,96 @@
             this.timer.Interval = 10;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(341, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "10";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(470, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(203, 57);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Event";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(137, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(316, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "100";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(253, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "ErrorCount";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(69, 153);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(44, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Log";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 443);
+            this.ClientSize = new System.Drawing.Size(599, 443);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
@@ -110,6 +203,14 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
 
--- [WinForm]SerialPortTimer/Form1.cs
+++ [WinForm]SerialPortTimer/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Text;
 using System.Windows.Forms;
 
 namespace _WinForm_SerialPortTimer
@@ -31,14 +32,26 @@ namespace _WinForm_SerialPortTimer
                 sPort.DtrEnable = true;
                 sPort.RtsEnable = true;
                 sPort.Open();
-                richTextBox1.AppendText(string.Format("[{0}][Connected] {1}{2}", DateTime.Now, comboBox1.Text, Environment.NewLine));
-                timer.Interval = 10;
-                timer.Start();
+                if (checkBox1.Checked)
+                {
+                    sPort.DataReceived += SPort_DataReceived;
+                }
+                else
+                {
+                    richTextBox1.AppendText(string.Format("[{0}][Connected] {1}{2}", DateTime.Now, comboBox1.Text, Environment.NewLine));
+                    timer.Interval = int.Parse(textBox2.Text);
+                    timer.Start();
+                }
             }
             catch (Exception ex)
             {
                 richTextBox1.AppendText(string.Format("[{0}][Error] {1}{2}", DateTime.Now, ex.Message, Environment.NewLine));
             }
+        }
+
+        private void SPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //getData();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -48,6 +61,7 @@ namespace _WinForm_SerialPortTimer
                 //sPort.DataReceived -= SPort_DataReceived;
                 sPort.Close();
                 timer.Enabled = false;
+                sPort.DataReceived -= SPort_DataReceived;
                 richTextBox1.AppendText(string.Format("[{0}][DisConnected] {1}{2}", DateTime.Now, comboBox1.Text, Environment.NewLine));
             }
             catch (Exception ex)
@@ -56,23 +70,47 @@ namespace _WinForm_SerialPortTimer
             }
         }
 
-        private void timer_Tick(object sender, EventArgs e)
-        {
-            //string test;
-            //test = sPort.ReadLine();
+        private int countBytesToReadLength = 0;
 
+        private void getData()
+        {
             try
             {
                 int bytes = sPort.BytesToRead;
-                byte[] buffer = new byte[bytes];
-                sPort.Read(buffer, 0, bytes);
-                string s = BitConverter.ToString(buffer);
-                textBox1.Text = s.Length.ToString() + "*" + s.Trim() + "*";
-                richTextBox1.AppendText(s.ToString() + Environment.NewLine);
+                if (bytes > 0)
+                {
+                    byte[] buffer = new byte[bytes];
+                    sPort.Read(buffer, 0, bytes);
+                    //string s = BitConverter.ToString(buffer);
+                    var s = Encoding.ASCII.GetString(buffer).Trim();
 
-
-
-
+                    //var s = BitConverter.ToInt32(buffer, 0).ToString(); //getbuffer);
+                    //textBox1.Text = s.Length.ToString() + "*" + s + "*";
+                    if (s.Length == 6)
+                    {
+                        textBox1.Text = double.Parse(s).ToString();
+                        countBytesToReadLength = 0;
+                    }
+                    else
+                    {
+                        countBytesToReadLength++;
+                    }
+                    if (checkBox2.Checked)
+                        richTextBox1.AppendText(bytes.ToString() + "-" + buffer.Length.ToString() + "-" + s.Length.ToString() + "=>" + s.ToString() + "<=" + Environment.NewLine);
+                }
+                else
+                {
+                    countBytesToReadLength++;
+                }
+                if (countBytesToReadLength >= int.Parse(textBox3.Text))
+                {
+                    textBox1.Text = "error";
+                }
+                label1.Text = countBytesToReadLength.ToString();
+                if (countBytesToReadLength >= int.Parse(label2.Text))
+                {
+                    label2.Text = countBytesToReadLength.ToString();
+                }
                 //double TrongLuong = 0;
                 //string data = sPort.ReadExisting();
                 //richTextBox1.AppendText(data.Trim() + Environment.NewLine);
@@ -101,7 +139,6 @@ namespace _WinForm_SerialPortTimer
                 //    }
                 //    textBox1.Text = TrongLuong.ToString();
                 //}
-
 
                 //StringBuilder rl = new StringBuilder(bytes * 2);
                 //string HexAlphabet = "1234567890ABCDEF";
@@ -149,6 +186,111 @@ namespace _WinForm_SerialPortTimer
             {
                 richTextBox1.AppendText(ex.Message + Environment.NewLine);
             }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //string test;
+            //test = sPort.ReadLine();
+            getData();
+            //try
+            //{
+            //    int bytes = sPort.BytesToRead;
+            //    byte[] buffer = new byte[bytes];
+            //    sPort.Read(buffer, 0, bytes);
+            //    //string s = BitConverter.ToString(buffer);
+            //    var s = Encoding.ASCII.GetString(buffer).Trim();
+
+            //    //var s = BitConverter.ToInt32(buffer, 0).ToString(); //getbuffer);
+            //    //textBox1.Text = s.Length.ToString() + "*" + s + "*";
+            //    if (s.Length == 6)
+            //    {
+            //        textBox1.Text = double.Parse(s).ToString();
+            //    }
+            //    richTextBox1.AppendText(s.Length.ToString()+ "=>" + s.ToString()+"<=" + Environment.NewLine);
+
+            //    //double TrongLuong = 0;
+            //    //string data = sPort.ReadExisting();
+            //    //richTextBox1.AppendText(data.Trim() + Environment.NewLine);
+            //    //if (data.Length > 0)
+            //    //{
+            //    //    textBox1.Text = data.Length.ToString() + "*" + data.Trim() + "*";
+            //    //}
+            //    //data = data.Remove(0, 1);
+            //    //if (data[0] == '+' || data[0] == '-')
+            //    //{
+            //    //    if (char.IsNumber(data[1]) && char.IsNumber(data[2]) && char.IsNumber(data[3]) && char.IsNumber(data[4]) && char.IsNumber(data[5]) && char.IsNumber(data[6]))
+            //    //    {
+            //    //        if (data[8] == '1')
+            //    //        {
+            //    //            if (data[0] == '-')
+            //    //            {
+            //    //                char[] c = { data[0], data[1], data[2], data[3], data[4], data[5], data[6] };
+            //    //                TrongLuong = Math.Round(double.Parse(new string(c)) / 1000, 3);
+            //    //            }
+            //    //            else
+            //    //            {
+            //    //                char[] c = { data[1], data[2], data[3], data[4], data[5], data[6] };
+            //    //                TrongLuong = Math.Round(double.Parse(new string(c)) / 1000, 3);
+            //    //            }
+            //    //        }
+            //    //    }
+            //    //    textBox1.Text = TrongLuong.ToString();
+            //    //}
+
+            //    //StringBuilder rl = new StringBuilder(bytes * 2);
+            //    //string HexAlphabet = "1234567890ABCDEF";
+            //    //foreach(byte b in buffer)
+            //    //{
+            //    //    rl.Append(HexAlphabet[(int)(b >> 4)]);
+            //    //    rl.Append(HexAlphabet[(int)(b & 0xF)]);
+            //    //}
+            //    //richTextBox1.AppendText(rl.ToString() + Environment.NewLine);
+
+            //    //string s = sPort.ReadExisting();
+            //    //richTextBox1.AppendText(((int)s[1]).ToString() +" "+ ((int)s[2]).ToString() + " " + ((int)s[3]).ToString() + " " + ((int)s[4]).ToString() + " " + ((int)s[5]).ToString() + " " + ((int)s[6]).ToString() + " " + ((int)s[7]).ToString() + " " + ((int)s[8]).ToString() + " " + ((int)s[9]).ToString() + " " + ((int)s[11]).ToString() + " " + ((int)s[10]).ToString() + Environment.NewLine);
+            //    //if (s[0] == '#' && s[10] == '*')
+            //    //{
+            //    //    if ((int)s[8] == 1)
+            //    //    {
+            //    //        CongTac1 = "1";
+            //    //        CongTac2 = "0";
+            //    //        DangCanBon = "1";
+            //    //    }
+            //    //    if ((int)s[9] == 1)
+            //    //    {
+            //    //        CongTac1 = "0";
+            //    //        CongTac2 = "1";
+            //    //        DangCanBon = "2";
+            //    //    }
+            //    //    txtTinHieuMain.Text = ((int)s[1]).ToString() + ((int)s[2]).ToString() + ((int)s[3]).ToString() + ((int)s[4]).ToString() + ((int)s[5]).ToString() + ((int)s[6]).ToString() + ((int)s[7]).ToString() + ((int)s[8]).ToString() + ((int)s[9]).ToString();
+            //    //    EpMain();
+            //    //    await Task.Delay(10);
+            //    //}
+            //    //sPort.Write("#");
+            //    //if (test[1] == '=')
+            //    //{
+            //    //   // double num = 0;
+            //    //    char[] c = new char[] { test[6], test[7], test[8], test[9] };
+            //    //    if (">;:73".IndexOf(test[2]) != -1)
+            //    //        textBox1.Text = (-1* double.Parse(0 + new string(c))).ToString();
+            //    //    else
+            //    //        textBox1.Text = (double.Parse(0 + new string(c))).ToString();
+            //    //    //textBox1.Text = new string(c);
+            //    //}
+            //    //richTextBox1.AppendText(test + Environment.NewLine);
+            //}
+            //catch (Exception ex)
+            //{
+            //    richTextBox1.AppendText(ex.Message + Environment.NewLine);
+            //}
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            label2.Text = "0";
+            textBox1.Text = "";
         }
     }
 }
